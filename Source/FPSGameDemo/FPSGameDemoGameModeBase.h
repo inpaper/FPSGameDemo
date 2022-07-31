2f5c07313273aebab8e25482bfcdb55bc5c9c87a
inpaper <1903825932@qq.com>
2022-07-31 15:26:50 +0800

    初始化项目

---
+++ Source/FPSGameDemo/FPSGameDemoGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FPSGameDemoGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FPSGAMEDEMO_API AFPSGameDemoGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
