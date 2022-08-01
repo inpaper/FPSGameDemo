09014782e960e0a5f4222001fe51fda131a3e287
inpaper <1903825932@qq.com>
2022-08-01 22:34:48 +0800

    1.实现了人物移动动画，包含四方移动和八方移动，目前八方移动的控制键设为ctrl键。 2.实现了炫光枪材质，以及添加了手电筒效果 3.动画还有开枪动画在实现开枪后补充进去 4.左上、右下同时动画移动效果较差，后续找到更好的素材可以更换

--- Source/FPSGameDemo/Private/MyCharacter.cpp
+++ Source/FPSGameDemo/Private/MyCharacter.cpp
@@ -3,18 +3,53 @@
 
 #include "FPSGameDemo/Public/MyCharacter.h"
 
+#include "Components/CapsuleComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
 // Sets default values
 AMyCharacter::AMyCharacter()
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	GetCapsuleComponent()->InitCapsuleSize(55.f, 96.0f);
+
+	GunComponent = CreateDefaultSubobject<USkeletalMeshComponent>("Gun");
+	GunSpotLightComponent = CreateDefaultSubobject<USpotLightComponent>("GunSpotLightComponent");
+	
+	// 锁定鼠标旋转，让鼠标旋转的时候不旋转人物
+	// bUseControllerRotationPitch = false;
+	// bUseControllerRotationYaw = false;
+	// bUseControllerRotationRoll = false;
+
+	// Configure character movement
+	// GetCharacterMovement()->bOrientRotationToMovement = true; // Character moves in the direction of input...	
+	// GetCharacterMovement()->RotationRate = FRotator(0.0f, 540.0f, 0.0f); // ...at this rotation rate
+	// GetCharacterMovement()->JumpZVelocity = 600.f;
+	// GetCharacterMovement()->AirControl = 0.2f;
+	
+	CameraComponent = CreateDefaultSubobject<UCameraComponent>("CameraComponent");
+	// CameraComponent->bUsePawnControlRotation = false;
+
+	CameraSpringArmComponent = CreateDefaultSubobject<USpringArmComponent>("CameraSpringArmComponent");
+	// TODO 设置不了初始旋转度数
+	const FRotator SpringRot = FRotator::FRotator(-20.0f,.0f,.0f);
+	CameraSpringArmComponent->SetRelativeRotation(SpringRot);
+	CameraSpringArmComponent->AttachTo(RootComponent);
+	CameraSpringArmComponent->TargetArmLength = 600.0f;
+	
+	
+	CameraSpringArmComponent->bUsePawnControlRotation = true;
+	CameraComponent->AttachTo(CameraSpringArmComponent);
 }
 
 // Called when the game starts or when spawned
 void AMyCharacter::BeginPlay()
 {
 	Super::BeginPlay();
+	if(GunComponent == nullptr)return;
+	GunComponent->AttachToComponent(GetMesh(), FAttachmentTransformRules(EAttachmentRule::SnapToTarget, true), TEXT("GunPoint"));
+	GunSpotLightComponent->AttachToComponent(GunComponent, FAttachmentTransformRules(EAttachmentRule::SnapToTarget, true), TEXT("LightPoint"));
 }
 
 // Called every frame
@@ -28,5 +63,71 @@ void AMyCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputCompone
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+	PlayerInputComponent->BindAxis("MoveForward",this,&AMyCharacter::MoveForward);
+	PlayerInputComponent->BindAxis("MoveRight",this,&AMyCharacter::MoveRight);
+
+	PlayerInputComponent->BindAxis("MouseX", this, &APawn::AddControllerYawInput);
+	PlayerInputComponent->BindAxis("MouseY", this, &APawn::AddControllerPitchInput);
+
+	// 实现了控制八方向移动情况。开发过程中先注释
+	PlayerInputComponent->BindAction("OnlyXOrY",IE_Pressed,this,&AMyCharacter::StartRunEight);
+	PlayerInputComponent->BindAction("OnlyXOrY",IE_Released,this,&AMyCharacter::StopRunEight);
+
+	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ACharacter::Jump);
+	PlayerInputComponent->BindAction("Jump", IE_Released, this, &ACharacter::StopJumping);
 }
+
+// 更改当前是否可以八方向移动的情况
+void AMyCharacter::StartRunEight()
+{
+	bRunEight = true;
+}
+
+void AMyCharacter::StopRunEight()
+{
+	bRunEight = false;
+}
+
+void AMyCharacter::MoveForward(float Value)
+{
+	if(GetController())
+	{
+		if(Value == 0.0f)
+		{
+			bRunY = false;
+			return;
+		}
+		if(bRunX && !bRunEight)return;
+		bRunY = true;
+		
+		const FRotator Rotation = GetController()->GetControlRotation();
+		const FRotator YawRotation(0, Rotation.Yaw, 0);
+		
+		const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+		AddMovementInput(Direction, Value);
+	}
+}
+
+void AMyCharacter::MoveRight(float Value)
+{
+	if ( GetController())
+	{
+		if(Value == 0.0f)
+		{
+			bRunX = false;
+			return;
+		}
+
+		if(bRunY && !bRunEight)return;
+		bRunX = true;
+		
+		const FRotator Rotation = GetController()->GetControlRotation();
+		const FRotator YawRotation(0, Rotation.Yaw, 0);
+		
+		const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+		AddMovementInput(Direction, Value);
+	}
+}
+
+
 
--- Source/FPSGameDemo/Public/MyCharacter.h
+++ Source/FPSGameDemo/Public/MyCharacter.h
@@ -3,7 +3,10 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "Camera/CameraComponent.h"
+#include "Components/SpotLightComponent.h"
 #include "GameFramework/Character.h"
+#include "GameFramework/SpringArmComponent.h"
 #include "MyCharacter.generated.h"
 
 UCLASS()
@@ -11,6 +14,17 @@ class FPSGAMEDEMO_API AMyCharacter : public ACharacter
 {
 	GENERATED_BODY()
 
+	UPROPERTY(EditAnywhere,Category="Gun")
+	USkeletalMeshComponent* GunComponent;
+
+	UPROPERTY(EditAnywhere,Category="Gun")
+	USpotLightComponent* GunSpotLightComponent;
+	
+	UPROPERTY(EditAnywhere,Category= "Camera")
+	UCameraComponent* CameraComponent;
+
+	UPROPERTY(EditAnywhere,Category="Camera")
+	USpringArmComponent* CameraSpringArmComponent;
 public:
 	// Sets default values for this character's properties
 	AMyCharacter();
@@ -19,11 +33,29 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+	void MoveForward(float Value);
+
+	void MoveRight(float Value);
+
+	// 更改当前是否可以八方向移动的情况。
+	void StartRunEight();
+	void StopRunEight();
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	
+private:
+	// 控制八方向移动，限制只有X和Y不能同时输入
+	bool bRunEight = false;
 
+	UBoolProperty* BRunEight;
+
+	// 判断是否当前正在按下X或者Y
+	bool bRunX = false;
+	bool bRunY = false;
+
+	
 };
